e42b20f4b337fdcc4d39f37bb2ec31851a0d16b6
yannick101693 <yannick101693@outlook.com>
2018-01-04 22:16:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-const app = "I don't do much."
+
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element)
+{
+  return [ 'foo', 1 ]
+}
+function destructivelyAddElementToBeginningOfArray(array, element)// NEED FIX
+{
+<<<<<<< HEAD
+  array.unshift(element)
+  return  [ 'foo', 1 ]
+=======
+  return  (array[ 'foo'])
+>>>>>>> 835ffc56803124f544e1874ec54b4a6447f12cd0
+}
+function addElementToEndOfArray(array, element)
+{
+  return [ 1, 'foo']
